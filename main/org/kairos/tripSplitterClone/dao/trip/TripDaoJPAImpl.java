47cda7bf55f989d369892ac7c09e1f738c57ed43
Axel Collard Bovy <axcbovy@gmail.com>
2015-08-31 09:38:11 -0300

    a

--- main/org/kairos/tripSplitterClone/dao/trip/TripDaoJPAImpl.java
+++ main/org/kairos/tripSplitterClone/dao/trip/TripDaoJPAImpl.java
@@ -1,7 +1,11 @@
 package org.kairos.tripSplitterClone.dao.trip;
 
 import org.kairos.tripSplitterClone.dao.AbstractDao;
+import org.kairos.tripSplitterClone.model.destination.City;
+import org.kairos.tripSplitterClone.model.destination.City_;
 import org.kairos.tripSplitterClone.model.trip.Trip;
+import org.kairos.tripSplitterClone.model.trip.Trip_;
+import org.kairos.tripSplitterClone.model.trip.UserTrip;
 import org.kairos.tripSplitterClone.model.user.User;
 import org.kairos.tripSplitterClone.utils.DozerUtils;
 import org.kairos.tripSplitterClone.vo.trip.TripVo;
@@ -11,9 +15,7 @@ import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -66,6 +68,34 @@ public class TripDaoJPAImpl extends AbstractDao<Trip, TripVo> implements I_TripD
 
 	@Override
 	public List<TripVo> usersTrip(EntityManager em, UserVo userVo) throws Exception {
+		this.logger.debug("getting all trips for user: {}", userVo.getEmail());
+
+		// builds the query
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Trip> query = builder.createQuery(this.getClazz());
+		Root<Trip> tripRoot = query.from(this.getClazz());
+		Join<Trip,UserTrip> userTripJoin = tripRoot.join(Trip_.travelers);
+
+		Predicate filters = builder.conjunction();
+
+		// filters by name
+		filters = builder.and(filters, builder.);
+		// filters by the deleted flag
+		filters = builder.and(filters, builder.equal(tripRoot.get(Trip_.deleted)
+				.as(Boolean.class), Boolean.FALSE));
+
+		query.where(filters);
+
+		try {
+			// fetch the document type
+			City city = em.createQuery(query).getSingleResult();
+
+			return this.map(city);
+		} catch (NoResultException e) {
+			// there was no city with required name
+			return null;
+		}
+
 		List<TripVo> tripVoList = new ArrayList<>();
 		try{
 			User user = em.find(User.class, userVo.getId());
